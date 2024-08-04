ccdf589244f8856cb455120f4e36f353fca9d3c7
Adarshkumar9934 <145029238+Adarshkumar9934@users.noreply.github.com>
2024-08-04 16:52:17 +0000

    Data types

---
+++ Datatypes.js
@@ -0,0 +1,26 @@
+let number = 34
+let state = " Jharkhand"
+let  s= true / false
+let x ,y,z;
+x =10;
+y =30;
+z =x+y;
+console.log(number);
+console.log(state);
+console.log(s);
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+//  number => 2 to power 53
+// Bigint
+// boolean=> true\ false
+// String=>" "
+// null=> StandAlon value
+// undefined => undefined value
+// symbol=>
+//  object=>
+
+console.log(typeof null);// object 
+console.log(typeof undefined);//undefined 
