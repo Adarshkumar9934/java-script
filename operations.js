df26a8c0b1149424c321aa9106a9deb261f394a2
Adarshkumar9934 <145029238+Adarshkumar9934@users.noreply.github.com>
2024-08-04 18:24:17 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,25 @@
+let str1 = "Hello";
+let Str2 ="Adash";
+let str3 = str1+Str2;
+console.log(str3);
+
+console.log(1 + 3);
+console.log(1+ "2");
+console.log("1"+2);
+console.log(1+2+"2");
+
+
+//  this are converstion but we can't  use in production base area and it is senseless  or  illogically code 
+console.log(+true);
+console.log(+false );
+console.log(+"");
+
+// Counter 
+
+let gamecounter = 110;
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
+console.log(gamecounter);
+
+
