a6e49b43bc318398ab7bb278e54de8f661753492
Adarshkumar9934 <145029238+Adarshkumar9934@users.noreply.github.com>
2024-08-04 15:42:49 +0000

    configure done

---
+++ variable.js
@@ -0,0 +1,15 @@
+const Name ="Adarsh"
+let title="yadav"
+let age=39;
+let address="delhi"
+console.table([address,age,Name,title])
+
+const Namea="Aman"
+title="Roy"
+age=9;
+address="Chirkund"
+console.table([address,age,Name,title,Namea])
+
+
+// In variable we are not useing var because of var is creating  a problem in scope so we are not using var
+//  we are useing const , let this are some variable we are using
