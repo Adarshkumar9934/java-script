e259187dfd31143f322fd37e3a05f02bc2c16bb5
Adarshkumar9934 <145029238+Adarshkumar9934@users.noreply.github.com>
2024-08-04 17:40:21 +0000

    converstion of datatype

---
+++ Operations.js
@@ -0,0 +1,29 @@
+let score = false
+
+/*console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+ 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+//  converstion
+// number will be the number =>33  convert => 33 number only.
+//  but will not convert it will shows as NaN (not a number). "33aba"=> NaN
+// bollean ture => 1 / false => 0
+
+
+let IsLoggedIn = "Adarsh";
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// Enter the value 1 => true & 0 => flase  but
+// Enter the empty string => 0
+// Enter the name in the String => 1  
+
+let SomeNumber =33
+let StringSomeNumber = String(SomeNumber);
+console.log(StringSomeNumber);
+console.log(typeof StringSomeNumber);
